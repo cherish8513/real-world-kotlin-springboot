841b03193b4b4366a8e48422f10f83522ffbc490
jooheon <cherish8513>
2025-08-15 23:43:30 +0900

    feat:initialize spring

---
+++ src/main/kotlin/com/joo/real_world/RealWorldApplication.kt
@@ -0,0 +1,11 @@
+package com.joo.real_world
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RealWorldApplication
+
+fun main(args: Array<String>) {
+	runApplication<RealWorldApplication>(*args)
+}
